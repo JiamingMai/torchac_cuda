225e8e6ddaa05c31c81d110adb401ce0c16ce314
ApostaC <jc4xvyp@outlook.com>
2024-09-19 22:21:30 -0500

    Add v0.2.5 with sdist

--- build-bdist.sh
+++ build-bdist.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
+#
+# Should be executed in docker: 
+#   docker pull sameli/manylinux_2_28_x86_64_cuda_12.3
+#   docker run -it --rm -v /path/to/your/package:/io sameli/manylinux_2_28_x86_64_cuda_12.3 /bin/bash
+
+set -e
+
+dnf install python3.11-devel wget
+wget https://bootstrap.pypa.io/get-pip.py
+python3 ./get-pip.py
+
+cd /io
 
 mkdir -p repaired
-
 rm -rf build/*
 rm -rf dist/*
 
-python setup.py sdist bdist_wheel
-auditwheel repair dist/torchac_cuda-0.1.0-cp310-cp310-linux_x86_64.whl -w repaired/
+pip install torch==2.4.0
+
+TORCH_CUDA_ARCH_LIST="8.0 8.6 8.9 9.0" python3 -m build --wheel --outdir dist/
+
+LD_LIBRARY_PATH=/usr/local/lib64/python3.11/site-packages/torch/lib/:/usr/local/lib/python3.11/site-packages/nvidia/nvtx/lib/:/usr/local/lib/python3.11/site-packages/nvidia/cudnn/lib/:/usr/local/lib/python3.11/site-packages/nvidia/cuda_cupti/lib/:/usr/local/lib/python3.11/site-packages/nvidia/nccl/lib auditwheel repair dist/*.whl -w /io/wheelhouse
--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from torch.utils import cpp_extension
 
 setup(
     name = 'torchac_cuda',
-    version = '0.1.0rc1',
+    version = '0.2.5',
     description = 'GPU based arithmetic coding for LLM KV compression',
     author = 'Yihua Cheng',
     author_email = 'yihua98@uchicago.edu',
+    include_package_data = True,
     ext_modules=[
         cpp_extension.CUDAExtension(
             'torchac_cuda', 
@@ -16,13 +17,18 @@ setup(
                 'torchac_kernel_dec_new.cu',
                 'cal_cdf.cu',
             ],
-            extra_compile_args={'cxx': ['-static-libgcc', '-static-libstdc++'],
-                                'nvcc': ['--compiler-options', "'-fPIC'"]},
+            extra_compile_args={
+                #'cxx': ['-static-libgcc', '-static-libstdc++'],
+                #'nvcc': ['--compiler-options', "'-fPIC'"]
+            },
             include_dirs=['./include']
             ),
         
     ],
     cmdclass={
         'build_ext': cpp_extension.BuildExtension
-    }
+    },
+    install_requires = [
+        "torch >= 2.1.0",
+    ]
 )
